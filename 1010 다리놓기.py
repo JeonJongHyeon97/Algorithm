adaec3e9434f90147598ab35d8d3aa906c73851d
JeonJongHyeon97 <67893559+JeonJongHyeon97@users.noreply.github.com>
2021-04-05 18:02:45 +0900

    Update 1010 다리놓기.py

--- 1010 다리놓기.py
+++ 1010 다리놓기.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  5 17:07:33 2021
 
-@author: user
-"""
+import math
 
+case_Num = int(input())
+
+for i in range(case_Num):
+    n,m = map(int, input().split())
+    # result = int(math.factorial(m)/math.factorial(n)/math.factorial(m-n))
+    result = int(math.factorial(m)//math.factorial(n)//math.factorial(m-n))
+    
+    print(result)
