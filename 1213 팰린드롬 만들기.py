6a3cf2a1ea687b5f5835ca658bcd56a16645450e
JeonJongHyeon97 <ziziwhdgus@naver.com>
2021-04-14 21:09:51 +0900

    dd

--- 1213 팰린드롬 만들기.py
+++ 1213 팰린드롬 만들기.py
@@ -1,8 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 14 16:41:09 2021
+임한수와 임문빈은 서로 사랑하는 사이이다.
 
-@author: JongHyeon
+임한수는 세상에서 팰린드롬인 문자열을 너무 좋아하기 때문에, 둘의 백일을 기념해서 임문빈은 팰린드롬을 선물해주려고 한다.
+
+임문빈은 임한수의 영어 이름으로 팰린드롬을 만들려고 하는데, 임한수의 영어 이름의 알파벳 순서를 적절히 바꿔서 팰린드롬을 만들려고 한다.
+
+임문빈을 도와 임한수의 영어 이름을 팰린드롬으로 바꾸는 프로그램을 작성하시오.
 """
+
+name = list(input())
+answer=[]
+odd=[]
+for i in name:
+   if name.count(i)%2 ==1:
+       if i*name.count(i) not in odd:
+           odd.append(i*name.count(i))
+
+   else: 
+       if i not in answer:
+           for j in range(name.count(i)//2):
+               answer.append(i)
+answer=sorted(answer)
+       
+if len(odd)>1:
+    print("I'm Sorry Hansoo")
+else:
+    print("".join(answer+odd+answer[::-1]))
 
 
--- 1235 학생번호.py
+++ 1235 학생번호.py
@@ -5,4 +5,24 @@ Created on Wed Apr 14 17:00:54 2021
 @author: JongHyeon
 """
 
+n = int(input())
+name =[]
+k =1
+for i in range(n):
+    name.append(input())
 
+tmp=[]
+for i in range(1,len(name[0])+1):
+    # print("i : ",i)
+    for j in range(len(name)):
+        # print("name : ", name[j])
+        # print("last : ", name[j][-i:])
+        if name[j][-i:] not in tmp:
+            tmp.append( name[j][-i:])
+        else:
+            j-=1
+            break
+    if( j == len(name)-1):
+        k=i
+        break
+print(k)
