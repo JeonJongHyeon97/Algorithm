924e9169b079efc90057d656ba0b962e4d71b28d
JeonJongHyeon97 <ziziwhdgus@naver.com>
2021-04-14 20:58:05 +0900

    asdf
    sfd

---
+++ 1213 팰린드롬 만들기.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 16:41:09 2021
+
+@author: JongHyeon
+"""
+
+
---
+++ 1244 스위치켜고끄기.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 17:18:02 2021
+
+@author: JongHyeon
+"""
+
+
