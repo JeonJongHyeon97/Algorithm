80df75b0061bf6d9ce119b0d87fe0324f5e5ee33
JeonJongHyeon97 <ziziwhdgus@naver.com>
2021-05-30 21:45:05 +0900

    gff

---
+++ 10974 모든수열.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 16:26:35 2021
+
+@author: JongHyeon
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 2003 수들의합2.py
+++ 2003 수들의합2.py
@@ -6,18 +6,19 @@ N개의 수로 된 수열 A[1], A[2], …, A[N] 이 있다.
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
-a = list(map(int, input().split()))
-count = 0
-total=0
-for i in range(0, n-1):
-    total=a[i]
-    if total == m:
-        count+=1
-    elif total < m:
-        for j in range(i+1, n):
-            if total+a[j] > m:
-                break
-            total += a[j]
-            if total == m:
-                count+=1
-print(count)
+A = list(map(int, input().split()))
+sum_A = [0] * (n + 1)
+for i in range(1, n + 1):
+    sum_A[i] = sum_A[i-1] + A[i-1]  
+answer = 0
+for i in range(n):
+    for j in range(i+1, n+1):
+        if sum_A[j] < m: 
+            pass
+        elif sum_A[j] - sum_A[i] > m:
+            break
+        elif sum_A[j] - sum_A[i] == m:
+            answer += 1
+            break
+            
+print(answer)    
